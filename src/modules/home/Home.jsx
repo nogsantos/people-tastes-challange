ac9ede20da54651e5d4459ad01e5d89731802f83
Fabricio Nogueira <nogsantos@gmail.com>
2019-02-25 22:33:05 -0300

    feat(lib): Image loader lib
    Try the lib to load images faster...

--- src/modules/home/Home.jsx
+++ src/modules/home/Home.jsx
@@ -5,8 +5,9 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import CardActionArea from '@material-ui/core/CardActionArea';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-import { Image } from './Style';
+import Img from 'react-image';
 
 const styles = theme => ({
 	root: {
@@ -20,7 +21,7 @@ const styles = theme => ({
 		padding: theme.spacing.unit * 2,
 		color: theme.palette.text.secondary,
 		position: 'relative',
-		height: '25rem',
+		height: '20rem',
 		width: '30rem',
 		textAlign: 'center',
 		'& > p': {
@@ -29,6 +30,19 @@ const styles = theme => ({
 			bottom: '10px',
 			backgroundColor: '#ffffff'
 		}
+	},
+	progress: {
+		margin: theme.spacing.unit * 2
+	},
+	img: {
+		position: 'relative',
+		width: '100%',
+		height: 'auto'
+	},
+	boxTitle: {
+		position: 'absolute',
+		width: '100%',
+		bottom: '15px'
 	}
 });
 class Home extends Component {
@@ -53,22 +67,30 @@ class Home extends Component {
 						<Grid item>
 							<CardActionArea onClick={() => this.goTo('swapi')}>
 								<Paper className={classes.paper}>
-									<Image
-										image="https://res.cloudinary.com/nogsantos/image/upload/v1551031396/people-tastes/Star-Wars.jpg"
-										title="Star Wars"
+									<Img
+										className={classes.img}
+										src={'https://res.cloudinary.com/nogsantos/image/upload/v1551031396/people-tastes/Star-Wars.jpg'}
+										loader={<CircularProgress className={classes.progress} />}
 									/>
-									<Typography variant="body1">Star Wars Movie</Typography>
+									<Typography variant="h4" className={classes.boxTitle}>
+										Star Wars Movie
+									</Typography>
 								</Paper>
 							</CardActionArea>
 						</Grid>
 						<Grid item>
 							<CardActionArea onClick={() => this.goTo('punkapi')}>
 								<Paper className={classes.paper}>
-									<Image
-										image="https://res.cloudinary.com/nogsantos/image/upload/v1551029618/people-tastes/original-beer.jpg"
-										title="Beers"
+									<Img
+										className={classes.img}
+										src={
+											'https://res.cloudinary.com/nogsantos/image/upload/v1551029618/people-tastes/original-beer.jpg'
+										}
+										loader={<CircularProgress className={classes.progress} />}
 									/>
-									<Typography variant="body1">Beers</Typography>
+									<Typography variant="h4" className={classes.boxTitle}>
+										Beers
+									</Typography>
 								</Paper>
 							</CardActionArea>
 						</Grid>
