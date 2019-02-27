7c62801b34fb5520c356c30505ecff7b9e3c34d0
Fabricio Nogueira <nogsantos@gmail.com>
2019-02-27 10:04:12 -0300

    refactor(favorite): Favorite Details
    Favorite details component

--- src/modules/my-favorites/MyFavorites.jsx
+++ src/modules/my-favorites/MyFavorites.jsx
@@ -1,29 +1,19 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 import { withRouter } from 'react-router';
-import teal from '@material-ui/core/colors/teal';
 import Divider from '@material-ui/core/Divider';
 
-import { CategoryIconsService, FavoriteService } from '../../services/';
+import { FavoriteService } from '../../services/';
+import MyFavoritesDetails from './MyFavoritesDetails';
 
 const styles = theme => ({
 	paper: {
 		padding: theme.spacing.unit * 2,
 		color: theme.palette.text.secondary,
 		position: 'relative'
-	},
-	icon: {
-		margin: theme.spacing.unit * 2,
-		color: teal[800]
 	}
 });
 class MyFavorites extends Component {
@@ -50,10 +40,6 @@ class MyFavorites extends Component {
 		});
 	};
 
-	goToDetailsView = (target, id) => {
-		this.props.history.push(`/${target}/details/${id}`);
-	};
-
 	render() {
 		const { classes } = this.props;
 		const { swapiList, punkapiList } = this.state;
@@ -64,49 +50,59 @@ class MyFavorites extends Component {
 					<Divider variant="fullWidth" />
 				</Grid>
 				<Grid item sm={6} xs={12}>
+					<Grid container direction="row" justify="space-between" alignItems="center" spacing={0}>
+						<Grid item sm={6} xs={12}>
+							<Typography variant="h6">Star Wars</Typography>
+						</Grid>
+						<Grid item sm={6} xs={12}>
+							<Typography variant="body1">Score: {swapiList.length}</Typography>
+						</Grid>
+					</Grid>
 					<Paper className={classes.paper}>
-						<List dense={false}>
-							{swapiList.length > 0 ? (
-								swapiList.map(swapi => {
-									return (
-										<Fragment>
-											<ListItem key={swapi.id} button onClick={() => this.goToDetailsView('swapi', swapi.id)}>
-												<FavoriteIcon className={classes.icon} />
-												<ListItemText primary={swapi.name} secondary={swapi.category} />
-												<ListItemAvatar>{new CategoryIconsService(swapi.category).getIcon()}</ListItemAvatar>
-											</ListItem>
-											<Divider variant="inset" component="li" />
-										</Fragment>
-									);
-								})
-							) : (
-								<Typography variant="body1">No Star wars items liked yet</Typography>
-							)}
-						</List>
+						{swapiList.length > 0 ? (
+							swapiList.map(swapi => {
+								return (
+									<MyFavoritesDetails
+										key={swapi.id}
+										id={swapi.id}
+										name={swapi.name}
+										module="swapi"
+										category={swapi.category}
+										showSecondaryText={true}
+										secondary={swapi.category}
+									/>
+								);
+							})
+						) : (
+							<Typography variant="body1">No Star wars items liked yet</Typography>
+						)}
 					</Paper>
 				</Grid>
 				<Grid item sm={6} xs={12}>
+					<Grid container direction="row" justify="space-between" alignItems="center" spacing={0}>
+						<Grid item sm={6} xs={12}>
+							<Typography variant="h6">Beers</Typography>
+						</Grid>
+						<Grid item sm={6} xs={12}>
+							<Typography variant="body1">Score: {punkapiList.length}</Typography>
+						</Grid>
+					</Grid>
 					<Paper className={classes.paper}>
-						<List dense={false}>
-							{punkapiList.length > 0 ? (
-								punkapiList.map(punkapi => {
-									return (
-										<Fragment>
-											<ListItem key={punkapi.id} button onClick={() => this.goToDetailsView('punkapi', punkapi.id)}>
-												<FavoriteIcon className={classes.icon} />
-												<ListItemText primary={punkapi.name} />
-												<ListItemAvatar>
-													<Avatar alt={punkapi.name} src={punkapi.image_url} />
-												</ListItemAvatar>
-											</ListItem>
-											<Divider variant="inset" component="li" />
-										</Fragment>
-									);
-								})
-							) : (
-								<Typography variant="body1">No beers liked yet</Typography>
-							)}
-						</List>
+						{punkapiList.length > 0 ? (
+							punkapiList.map(punkapi => {
+								return (
+									<MyFavoritesDetails
+										key={punkapi.id}
+										id={punkapi.id}
+										name={punkapi.name}
+										module="punkapi"
+										image_url={punkapi.image_url}
+									/>
+								);
+							})
+						) : (
+							<Typography variant="body1">No beers liked yet</Typography>
+						)}
 					</Paper>
 				</Grid>
 			</Grid>
