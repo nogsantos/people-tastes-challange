82b517fa4130b4f7e79c468b174ea61b25cf6b05
Fabricio Nogueira <nogsantos@gmail.com>
2019-02-28 11:27:06 -0300

    refactor(component): Removed component remove referece from index

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { default as Menu } from './menu/';
 export { default as Bar } from './bar/';
-export { default as AppChips } from './app-chips/';
 export { default as AppInput } from './app-input/';
 export { default as AppDrawer } from './app-drawer/';
 export { default as AppSnackBar } from './app-snackbar/';
